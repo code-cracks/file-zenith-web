ccfbf7230bd6436182ec0e34d1fcd0cf1aef7a55
cssMagic <1207588952@qq.com>
2025-03-20 23:52:55 +0800

    feat(TitleNav，page): 🚀 添加头部导航栏，兼容移动端
    TitleNav组件，tw的darkmode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import TitleNav from '../components/TitleNav';
 import ThemeButton from '@/components/ThemeButton';
 
 export default function Home() {
   return (
     <div>
+      <TitleNav></TitleNav>
       file-zenith-web
       <ThemeButton />
     </div>

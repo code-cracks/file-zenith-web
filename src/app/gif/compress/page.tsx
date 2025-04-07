10a9b7c964957bd44be147290fdfcec2a96d1098
wangcheng215 <wangcheng215@ke.com>
2025-04-07 18:32:03 +0800

    fix: 修复组件目录名

--- src/app/gif/compress/page.tsx
+++ src/app/gif/compress/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import GifBatchCompressor from './components/GifBatchCompressor';
+import GifBatchCompressor from './_components/GifBatchCompressor';
 
 const GifCompress = () => {
   return (

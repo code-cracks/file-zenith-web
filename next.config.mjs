9a9f00ffa829a445da8177e124a95e8fcaa21c85
GrapeWell <1016939850@qq.com>
2025-04-22 15:21:55 +0800

    feat: 图片智能抠图

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,17 @@ const nextConfig = {
     config.externals = [...config.externals, 'sharp'];
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/image/:path*',
+        headers: [
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
